1f2f6bb4150eb62bfcf0f456b92e56ea15111785
Leandro <leaqjandro@gmail.com>
2020-06-09 17:22:31 -0600

    Ver dbuser main

---
+++ app/src/main/java/com/example/desticaaplicacion/ConnectionClass.java
@@ -0,0 +1,46 @@
+package com.example.desticaaplicacion;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionClass {
+
+    String classs = "com.mysql.jdbc.Driver";
+
+    String url = "jdbc:mysql://163.178.107.10/dbdestica";
+    String un = "laboratorios";
+    String password = "UCRSA.118";
+
+
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+
+            Class.forName(classs);
+
+            conn = DriverManager.getConnection(url, un, password);
+
+
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO1", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO2", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO3", e.getMessage());
+        }
+        return conn;
+    }
+
+}
--- app/src/main/java/com/example/desticaaplicacion/MainActivity.java
+++ app/src/main/java/com/example/desticaaplicacion/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.desticaaplicacion;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -18,16 +22,27 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    ConnectionClass connectionClass;
+    TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        texto = (TextView)findViewById(R.id.textbd);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -41,6 +56,95 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+        //bd action
+        final Button btnPrueba = (Button) findViewById(R.id.btnPrueba);
+
+
+        btnPrueba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //MainActivity.Doregister doregister = new MainActivity.Doregister();
+
+                //doregister.execute("");
+
+                connectionClass = new ConnectionClass();
+                new getCurso().execute();
+            }
+        });
+
+    }
+
+    public class getCurso extends AsyncTask<String,Void, ResultSet> {
+
+        @Override
+        protected ResultSet doInBackground(String... strings) {
+
+            try {
+
+                Connection con = connectionClass.CONN();
+
+                Statement estado = con.createStatement();
+
+                String peticion ="SELECT * FROM dbdestica.tbuser limit 1";
+                ResultSet result = estado.executeQuery(peticion);
+
+                return result;
+            } catch (SQLException e) {
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ResultSet result) {
+
+            try {
+
+
+                while (result.next()) {
+                    texto.setText(result.getString("email"));
+
+                }
+
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public class Doregister extends AsyncTask<String,String,String>
+    {
+        @Override
+        protected String doInBackground(String... params) {
+
+
+            try {
+                Connection con = connectionClass.CONN();
+
+                String query="SELECT * FROM dbdestica.tbuser limit 1;";
+
+                Statement stmt = con.createStatement();
+                stmt.executeUpdate(query);
+
+                Context context = getApplicationContext();
+                CharSequence text = "Curso Agregado";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+            catch (Exception ex)
+            {
+                Log.e("ERRORR", ex.getMessage());
+            }
+            return "";
+        }
     }
 
     @Override
@@ -56,4 +160,6 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
 }
