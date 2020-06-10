e154f3f1d318f7c8ec67fd39d8880a89b8cfe9dc
Leandro <leaqjandro@gmail.com>
2020-06-10 00:18:42 -0600

    Insertar usuario

--- app/src/main/java/com/example/desticaaplicacion/ui/login/RegistroMain.java
+++ app/src/main/java/com/example/desticaaplicacion/ui/login/RegistroMain.java
@@ -1,12 +1,16 @@
 package com.example.desticaaplicacion.ui.login;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -27,6 +31,13 @@ public class RegistroMain extends Fragment {
 
     private RegistroViewModel registroViewModel;
     ConnectionClass connectionClass;
+    EditText user;
+    EditText password1;
+    EditText password2;
+    String txtUser;
+    String txtPass1;
+    String txtPass2;
+    TextView edit2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,12 +45,21 @@ public class RegistroMain extends Fragment {
                 ViewModelProviders.of(this).get(RegistroViewModel.class);
         View root = inflater.inflate(R.layout.layout_registro, container, false);
 
+        user = root.findViewById(R.id.etNombre);
+        password1 = root.findViewById(R.id.etpassword);
+        password2 = root.findViewById(R.id.etpassword2);
 
         Button btnRegister = root.findViewById(R.id.btnRegister);
+       edit2= root.findViewById(R.id.edit);
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                txtUser= user.getText().toString();
+                txtPass1= password1.getText().toString();
+                txtPass2= password2.getText().toString();
+
 
                 connectionClass = new ConnectionClass();
                 new RegistroMain.setUser().execute();
@@ -47,28 +67,35 @@ public class RegistroMain extends Fragment {
         });
         return root;
     }
-    public class setUser extends AsyncTask<String,Void, ResultSet> {
+    public class setUser extends AsyncTask<String,String,String> {
 
         @Override
-        protected ResultSet doInBackground(String... strings) {
-
+        protected String doInBackground(String... strings) {
             try {
-
                 Connection con = connectionClass.CONN();
+                edit2.setText(txtUser);
+                String query="INSERT INTO tbuser \n" +
+                        "(nameUser,\n" +
+                        "passwordUser)\n" +
+                        "VALUES\n" +
+                        "('"+txtUser+"','"+txtPass1+"');";
 
-                Statement estado = con.createStatement();
-                String peticion ="SELECT * FROM dbdestica.tbuser limit 1";
-                estado.executeQuery(peticion);
+                Statement stmt = con.createStatement();
+                stmt.executeUpdate(query);
 
-            } catch (SQLException e) {
 
             }
-            return null;
+            catch (Exception ex)
+            {
+                Log.e("ERRORR", ex.getMessage());
+
+
+            }
+
+            return "";
+
         }
 
-        @Override
-        protected void onPostExecute(ResultSet result) {
 
-        }
     }
 }
