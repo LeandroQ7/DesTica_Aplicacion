f56c5778f3a01a2fec87c0cd2171cc0a589e4392
Anthony S. A <asalazar0592@gmail.com>
2020-06-11 01:11:42 -0600

    Cambios Encuesta

--- app/src/main/java/com/example/desticaaplicacion/ui/encuesta/EncuestaMain.java
+++ app/src/main/java/com/example/desticaaplicacion/ui/encuesta/EncuestaMain.java
@@ -1,29 +1,76 @@
 package com.example.desticaaplicacion.ui.encuesta;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import com.example.desticaaplicacion.*;
 
 import com.example.desticaaplicacion.R;
+
+import java.sql.Connection;
+import java.sql.Statement;
 
 public class EncuestaMain extends Fragment {
 
     private EncuestaViewModel homeViewModel;
+    ConnectionClass connectionClass;
+    EditText puntos;
+    EditText comentario;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(EncuestaViewModel.class);
+    String txtpuntos;
+    String txtcomentario;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //homeViewModel = ViewModelProviders.of(this).get(EncuestaViewModel.class);
         View root = inflater.inflate(R.layout.layout_encuesta, container, false);
 
+        // get selected radio button from radioGroup
+        RadioGroup gender;
+        //puntos = gender.getCheckedRadioButtonId();
+        comentario = root.findViewById(R.id.editText2);
+        Button buttonencuesta = root.findViewById(R.id.buttonencuesta);
+
+        buttonencuesta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        //txtpuntos= puntos.getText().toString();
+                        txtcomentario = comentario.getText().toString();
+                        
+                        connectionClass = new ConnectionClass();
+                        new EncuestaMain.setEncuesta().execute();
+            }
+        });
+
+
         return root;
+    }
+
+    public class setEncuesta extends AsyncTask<String,String,String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                Connection con = connectionClass.CONN();
+                comentario.setText(txtcomentario);
+                String query = "INSERT INTO setEncuesta \n" + "(calification,\n" + "opinion)\n" + "VALUES\n" + "(" + 1 + ",'" + txtcomentario + "');";
+                Statement stmt = con.createStatement();
+                stmt.executeUpdate(query);
+            } catch (Exception ex) {
+                Log.e("ERRORR", ex.getMessage());
+            }
+            return "";
+
+        }
     }
 }
